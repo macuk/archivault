1deb1917bf5d972e7152f90b2a102c3104f3f511
Piotr Macuk <piotr@macuk.pl>
2026-04-08 18:08:40 +0200

    Setup

--- Gemfile
+++ Gemfile
@@ -11,3 +11,7 @@ gem "rake", "~> 13.0"
 gem "minitest", "~> 5.16"
 
 gem "rubocop", "~> 1.21"
+
+gem "rubocop-minitest", "~> 0.39.1"
+
+gem "rubocop-rake", "~> 0.7.1"
--- test/test_archivault.rb
+++ test/test_archivault.rb
@@ -6,8 +6,4 @@ class TestArchivault < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Archivault::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
